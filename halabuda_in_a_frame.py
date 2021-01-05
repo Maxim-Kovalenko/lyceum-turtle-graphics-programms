1c7c3422a00222c413710d083228cd597984d7b5
Max <mx.a.kovalenko@gmail.com>
2021-01-05 19:13:22 +0200

    reforms
    New reforms in halabuda_in_a_frame.py

--- halabuda_in_a_frame.py
+++ halabuda_in_a_frame.py
@@ -1,13 +1,22 @@
 from turtle import *
 from random import *
 
+
+def move(xstart, ystart):
+    penup()
+    goto(xstart, ystart)
+    pendown()
+
+
 def moveDelta(x, y):
     penup()
     goto(int(xcor()) + x, int(ycor()) + y)
     pendown()
 
+
 def moveDeltaAndPaint(x, y):
     goto(int(xcor()) + x, int(ycor()) + y)
+
 
 def frame(height):
     pensize(5)
@@ -21,6 +30,7 @@ def frame(height):
     moveDeltaAndPaint(0, x)
     moveDelta(-2 * x, -x)
 
+
 def square(side):
     pensize(1)
     pencolor(choice(["green", "blue", "yellow"]))
@@ -31,6 +41,7 @@ def square(side):
     moveDeltaAndPaint(-side, 0)
     moveDeltaAndPaint(0, -side)
     end_fill()
+
 
 def triangle(side):
     pensize(3)
@@ -45,6 +56,7 @@ def triangle(side):
     left(120)
     end_fill()
 
+
 def house(size, forwardHeight):
     penup()
     left(90)
@@ -56,10 +68,12 @@ def house(size, forwardHeight):
     triangle(size)
 
 
-def picture(height):
+def picture(height, xstart, ystart):
+    move(xstart, ystart)
     frame(height)
     house(height / 4, height / 4)
 
+
 speed(5)
-picture(int(input("Введіть висоту вашого зображення: ")))
+picture(int(input("Введіть висоту вашого зображення: ")), int(input("Уведіть початковий X: ")), int(input("Уведіть початковий Y: ")))
 
